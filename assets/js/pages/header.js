c672c253891ebfb43882567bf74b2818a8f1103e
Bojaniko <121692108+Bojaniko@users.noreply.github.com>
2024-11-04 22:53:14 +0100

    responsive v1

--- assets/js/pages/header.js
+++ assets/js/pages/header.js
@@ -3,6 +3,26 @@ export function initHeader() {
     window.addEventListener("scroll", () => {
         handleHeaderScroll();
     });
+
+    initHamburger();
+}
+
+let hamburger;
+let hamburgerMenu;
+let hamburgerMenuCloser;
+
+function initHamburger() {
+	hamburger = header.querySelector(".wrapper").querySelector(".hamburger");
+	hamburgerMenu = header.querySelector(".hamburger-menu");
+	hamburgerMenuCloser = hamburgerMenu.querySelector(".close");
+	hamburger.addEventListener("click", () => {
+		tools.showElement(hamburger, false);
+		tools.showElement(hamburgerMenu, true);
+	});
+	hamburgerMenuCloser.addEventListener("click", () => {
+		tools.showElement(hamburger, true);
+		tools.showElement(hamburgerMenu, false);
+	});
 }
 
 function handleHeaderScroll() {
