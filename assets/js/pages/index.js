32a60d0d01758daebd83d98a26473b5a62214d38
Bojaniko <121692108+Bojaniko@users.noreply.github.com>
2024-11-24 01:40:06 +0100

    working

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -5,4 +5,36 @@ window.addEventListener("load", () => { initIndexPage(); });
 function initIndexPage() {
     _page.initPage();
     _page.scrollHandler.handlePageScrolling();
+
+    initFixedScrolling();
+}
+
+let currentScrolledTo = 0;
+let mainElement;
+
+function initFixedScrolling() {
+    mainElement = document.querySelector("main");
+    scrollToChild(0);
+    document.addEventListener("wheel", (e) => {
+        if (e.deltaY < 0) {
+            if (currentScrolledTo == 0) {
+                return;
+            }
+            currentScrolledTo--;
+            scrollToChild(currentScrolledTo);
+        }
+        else if (e.deltaY > 0) {
+            if (currentScrolledTo == mainElement.children.length - 1) {
+                return;
+            }
+            currentScrolledTo++;
+            scrollToChild(currentScrolledTo);
+        }
+    });
+}
+
+function scrollToChild(index) {
+    mainElement.children[index].scrollIntoView({
+        behaviour: "smooth", block: "start", inline: "start"
+    });
 }
